9e0d62bd62c002ed62ea4fdf19b3aca4357d4043
Adya-git13 <82499060+Adya-git13@users.noreply.github.com>
2023-03-16 16:08:59 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,7 +6,8 @@ predictionfile = "prediction_branch_sliding.csv"
 actualreader = pd.read_csv(actualfile, usecols = ['ROLL NO.','CURRENT BRANCH','ALLOTTED BRANCH'])
 predreader = pd.read_csv(predictionfile, usecols = ['ROLL NO.','CURRENT BRANCH','ALLOTTED BRANCH'])
 
-c_result = actualreader[~actualreader.apply(tuple,1).isin(predreader.apply(tuple,1))]
+c_result = actualreader[~actualreader.apply(tuple,1).isin(predreader.apply(tuple,1))] + predreader[~predreader.apply(tuple,1).isin(actualreader.apply(tuple,1))]
 print(c_result)          
+      
             
             
